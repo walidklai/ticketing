20b4c95e45dc7b39dd4dac84846d7062398a2c76
walidklai <walid.klai@gmail.com>
2022-05-11 20:35:55 +0100

    sign in done

--- auth/src/middlewares/inputsValidation.ts
+++ auth/src/middlewares/inputsValidation.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../../errors/request-validation-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from "express";
-import { basicCredentialsValidation } from "../validation/inputs/auth-inputs";
+import { BadRequestError } from "../errors/bad-request-error";
+import { basicCredentialsValidation } from "../middlewares/inputsValidation";
+import { User } from "../models/UserModel";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -7,7 +11,28 @@ router.post(
   "/signin",
   basicCredentialsValidation,
   (req: Request, res: Response) => {
-    res.send("yes")
+    const { email, password } = req.body;
+
+    const userExists = User.findOne({ email });
+
+    if (!userExists) throw new BadRequestError("Wrong Email or Password");
+
+    const passwordMatch = Password.compare(userExists.password, password);
+
+    if (!passwordMatch) throw new BadRequestError("Wrong Email or Password");
+
+    const payload = {
+      id: userExists.id,
+      email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET!);
+
+    req.session = {
+      jwt: token,
+    };
+
+    res.status(200).send(userExists);
   }
 );
 
