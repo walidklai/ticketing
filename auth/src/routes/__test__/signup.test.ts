9275ac0800d433448530122165e6f6c1a520fdbf
walidklai <walid.klai@gmail.com>
2022-05-15 16:30:51 +0100

    test pre commit

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,53 +2,45 @@ import request from "supertest";
 import { User } from "../../models/UserModel";
 import { app } from "../../app";
 
-it("Should return 201 after signing up a new user", async () => {
-  const newUser = {
+describe("Sign up process", () => {
+  const validUser = {
     email: "test@test.com",
     password: "12345678",
   };
-  await request(app).post("/api/users/signup").send(newUser).expect(201);
-});
-
-it("Should return 400 with invalid email message", async () => {
-  const newUser = {
+  const invalidEmailUser = {
     email: "testtest.com",
     password: "12345678",
   };
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send(newUser)
-    .expect(400);
-  expect(response.body).toEqual(
-    expect.objectContaining({
+
+  it("Should return 201 after signing up a new user", async () => {
+    await request(app).post("/api/users/signup").send(validUser).expect(201);
+  });
+
+  it("Should return 400 with invalid email message", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(invalidEmailUser)
+      .expect(400);
+    expect(response.body).toEqual({
       errors: [
         { message: "Please enter a valid email address", param: "email" },
       ],
-    })
-  );
-});
+    });
+  });
 
-it("Should set a cookie", async () => {
-  const newUser = {
-    email: "test@test.com",
-    password: "12345678",
-  };
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send(newUser)
-    .expect(201);
-  expect(response.get("Set-Cookie")).toBeDefined();
-});
+  it("Should set a cookie", async () => {
+    const response = await request(app)
+      .post("/api/users/signup")
+      .send(validUser)
+      .expect(201);
+    expect(response.get("Set-Cookie")).toBeDefined();
+  });
 
-it("Should call user find", async () => {
-  const newUser = {
-    email: "test@test.com",
-    password: "12345678",
-  };
+  it("Should call user find", async () => {
+    const spyOnFindOne = jest.spyOn(User, "findOne");
 
-  const spyOnFindOne = jest.spyOn(User, "findOne");
-  await request(app).post("/api/users/signup").send(newUser);
-
-  expect(spyOnFindOne).toHaveBeenCalledTimes(1);
-  expect(spyOnFindOne).toBeCalledWith({ email: "test@test.com" });
+    await request(app).post("/api/users/signup").send(validUser);
+    expect(spyOnFindOne).toHaveBeenCalledTimes(1);
+    expect(spyOnFindOne).toBeCalledWith({ email: "test@test.com" });
+  });
 });
